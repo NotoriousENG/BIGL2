e2acccc5f6582c870942bce8c103df0151c3c116
Michael O'Connell <michaeljoconnell@ufl.edu>
2018-04-22 19:43:32 -0400

    Update main.cpp
    Comments are added, most weird white space is gone, make sure you still add in the fancy comment I added on your Mac

--- Game/main.cpp
+++ Game/main.cpp
@@ -8,33 +8,40 @@ int main(int argc, const char * argv[]) {
     int lives = 0;
     std::cout<<"How many games do you want to play?"<<std::endl;
     std::cin>>lives;
+    
+    //Error handling for wrong variable type input
+    //Also handles too large/small inputs i.e. 2^31
     while(std::cin.fail()) {
         std::cout << "Error. Try again. Please enter int value 1-99" << std::endl;
         std::cin.clear();
         std::cin.ignore(256,'\n');
         std::cin >> lives;
     } //floors decimals
+    
+    //Catches if user input is too large
     if (lives>99){
         std::cout<<"Max 99 lives. Lives is now equal to 99."<<std::endl;
         lives=99;
     }
+    //Catches negative user input
     if(lives<1){
         std::cout<<"Min 1 life. Lives is now equal to 1."<<std::endl;
         lives=1;
     }
     
-    
-    
-    //added score
+    //Creation of a score array to store the score of a game
     int *scores = new int[lives];
+    
+    // lives is the ammount of times the game is to be played
     int i = lives;
+    
+    // we need to save the initial number of lives for later use
     int initLives = lives;
     
-    
-    
-    
+    //while there are still more lives (plays) left
     while(lives!=0){
     
+    //fps - frames per second
     const int fps = 60;
    
     const int frameDelay = 1000/fps;
@@ -44,19 +51,15 @@ int main(int argc, const char * argv[]) {
     
     game = new Game();
     
+    // game window logic
     game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, false);
-    
-    
-    
-    
     
     
     while (game->running()){
         
+        //how long the game is going 
+        // 1 tick = 1 millisecond
         frameStart = SDL_GetTicks();
-        
-        
-        
         
         bool alive = false;
         bool bigalive = false;
@@ -65,9 +68,7 @@ int main(int argc, const char * argv[]) {
         game->render();
         alive = game->checkCollision();
         bigalive = game->checkBigCollision();
-        
-        
-        
+
         
         frameTime = SDL_GetTicks() - frameStart;
         
@@ -75,45 +76,50 @@ int main(int argc, const char * argv[]) {
             SDL_Delay(frameDelay - frameTime);
         }
         
+        //When you die, the ammount of times you can play is decrimented
         if ((alive == true)||(bigalive == true)){
             lives--;
-            std::cout<<"You died."<<std::endl;
+            std::cout<<"You died, better luck next time!"<<std::endl;
             break;
         }
         
         
         
     }
+        // when you die the score is recorded in an array and printed
+        // and the game restarts
         scores[initLives-i] = SDL_GetTicks()/1000;
         std::cout<< scores[initLives-i]<<std::endl;
         i--;
         game->clean();
     }
     
+    // temporary High Score variable
     int temp = 0;
     
+    // we print each score
         for(int i = 0; i< initLives; i++){
-        
                 std::cout<<scores[i]<<std::endl;
-        
             }
     
+    // we will find the highest score
+    // we will look through the entire array of scores
         for(int k = 0; k< initLives; k++){
-        
-                
-        
+            // if the score is greater than temp
                 if(scores[k] > temp){
-            
+            // we overwrite temp with the higher score
                         temp = scores[k];
             
                     }
         
             }
+    // game loop is over
     
+    // We print the high score
         std::cout << "High score: " << temp << std::endl;
     
     
     
-    
+    // We exit the program
     return 0;
 }
