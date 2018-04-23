ff83b4d25a6c110a455c4bb8f93e6fdc7737c7e6
Michael O'Connell <michaeljoconnell@ufl.edu>
2018-04-22 22:17:53 -0400

    Update BigFuckingRock.hpp
    Added Comments

--- Game/BigFuckingRock.hpp
+++ Game/BigFuckingRock.hpp
@@ -3,9 +3,18 @@
 #define BigFuckingRock_hpp
 #include "Game.hpp"
 
-#include <stdio.h>
+#include <stdio.h> // include standard input output header
+
+/* Here we are creating The BigFuckingRock Class
+ * BigFuckingRock is an AI component that will track player actions
+ * it is designed to make the game more difficult 
+ * it will track the player's location which will prevent the player 
+ * from staying in the same space
+ */
 
 class BigFuckingRock{
+    
+    //Public: Methods go here
 public:
     BigFuckingRock(const char* texturesheet, int x, int y);
     ~BigFuckingRock();
@@ -16,7 +25,7 @@ public:
     int yval();
     
     
-    
+    //Private: To be initialized goes here
 private:
     int xpos;
     int ypos;
